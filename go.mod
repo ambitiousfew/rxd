cd092ff88d73f6f49d74e8369c66d9216063d7f0
Chris Howell <4532318+crhowell@users.noreply.github.com>
2024-01-03 07:35:30 -0800

    add some basic tests in for service context and service states; update intracom to v1.1.2

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.20
 
 require golang.org/x/exp v0.0.0-20231219180239-dc181d75b848
 
-require github.com/ambitiousfew/intracom v1.1.0
+require github.com/ambitiousfew/intracom v1.1.2
--- service_context_test.go
+++ service_context_test.go
@@ -1,1 +1,42 @@
 package rxd
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestServiceShutdownContext(t *testing.T) {
+	// we cancel the service context immediately so it should always run first.
+	testCtx, cancel := context.WithCancel(context.Background())
+
+	vs := &validService{}
+	opts := NewServiceOpts(UsingContext(testCtx))
+	validSvc := NewService("valid-service", vs, opts)
+
+	cancel()
+
+	timer := time.NewTimer(50 * time.Millisecond)
+	defer timer.Stop()
+	select {
+	case <-validSvc.ShutdownCtx.Done():
+		return
+	case <-timer.C:
+		t.Errorf("ServiceContext ShutdownSignal did not properly trigger")
+	}
+}
+
+func TestServiceContextShutdownSuccessful(t *testing.T) {
+	vs := &validService{}
+	opts := NewServiceOpts()
+	validSvc := NewService("valid-service", vs, opts)
+	// Ensure service has a logC so it wont hang on channel based logging.
+
+	validSvc.shutdown()
+	want := int32(1)
+	got := validSvc.shutdownCalled.Load()
+
+	if got != want {
+		t.Errorf("ServiceContext shutdown() did not shutdown properly, wanted %d for shutdownCalled got %d", want, got)
+	}
+}
