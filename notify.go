39a8dfcf055382ae40d341c262a1b53fee8d8f6c
Chris Howell <crhowell@users.noreply.github.com>
2024-07-07 09:08:18 -0700

    add future todo comment for notify.go

--- notify.go
+++ notify.go
@@ -2,6 +2,21 @@ package rxd
 
 import "context"
 
+// TODO: This is a basic implementation for interacting with a service manager.
+// Actual interactions with a service manager are a little more involved.
+// This implementation really only provides a way to notify watchdog if its enabled.
+// This is not cross-platform and is only for linux systems that use systemd.
+//
+// Ideally there should be a subpackage that provides an interface for running
+// as a system daemon on different platforms. This subpackage would need to
+// provide a factory that can hand back a given struct that meets the interface
+// based on the runtime value(s).
+//
+// Basically if we build with linux tags, we get the systemd implementation.
+// if we build with windows tags, we get the windows service implementation.
+// Because I want rxd to be cross-platform, this is already a consideration
+// for the future. Currently its a big lift and current needs are only for linux.
+
 type SystemNotifier interface {
 	Start(ctx context.Context, errC chan<- DaemonLog) error
 	Notify(state NotifyState) error
